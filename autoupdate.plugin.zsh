0b989557c27e882dec1d302325b85883a8266086
Bostjan H <hribcek@gmail.com>
2025-12-02 20:19:34 +0000

    fix: improve readability and consistency in autoupdate.plugin.zsh

--- autoupdate.plugin.zsh
+++ autoupdate.plugin.zsh
@@ -1,11 +1,11 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # self-update
 
-if which tput >/dev/null 2>&1; then
+if which tput &>/dev/null ; then
   ncolors=$(tput colors)
 fi
 
-if [[ $- == *i* ]] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+if [[ ${-} == *i*  &&  -n "${ncolors}"  &&  "${ncolors}" -ge 8 ]]; then
   RED="$(tput setaf 1)"
   BLUE="$(tput setaf 4)"
   GREEN="$(tput setaf 2)"
@@ -20,7 +20,7 @@ fi
 zmodload zsh/datetime
 
 function _current_epoch() {
-  echo $(( $EPOCHSECONDS / 60 / 60 / 24 ))
+  echo $(( EPOCHSECONDS / 86400 ))
 }
 
 function _update_zsh_custom_update() {
@@ -28,118 +28,121 @@ function _update_zsh_custom_update() {
 }
 
 function _get_epoch_target() {
-  local epoch_target
+  local epoch_target_value
 
-  zstyle -g epoch_target ':omz:update' frequency \
-    || epoch_target="$UPDATE_ZSH_DAYS"
-  if [[ -z "$epoch_target" ]]; then
-    # Default to old behavior
-    epoch_target=13
+  if ! zstyle -g epoch_target_value ':omz:update' frequency ; then
+    epoch_target_value="${UPDATE_ZSH_DAYS}"
   fi
 
-  echo "$epoch_target"
+  # Default to old behavior
+  epoch_target_value=${epoch_target_value:-13}
+
+  echo "${epoch_target_value}"
 }
 
 epoch_target="$(_get_epoch_target)"
 
 function _upgrade_custom_plugin() {
   # path of plugin/theme
-  p=$(dirname "$d")
-  # it's name
-  pn=$(basename "$p")
-  # it's type (plugin/theme)
-  pt=$(dirname "$p")
-  pt=$(basename ${pt:0:((${#pt} - 1))})
+  fullpath=$(dirname "${1}")
+  # its name
+  pathname=$(basename "${fullpath}")
+  # its type (plugin/theme)
+  pathtype=$(dirname "${fullpath}")
+  pathtype=$(basename ${pathtype:0:((${#pathtype} - 1))})
 
-  last_head=$( git -C "${p}" rev-parse HEAD )
-  if git -C "${p}" pull --quiet --rebase --stat --autostash
-  then
-    curr_head=$( git -C "${p}" rev-parse HEAD )
-    if [ "${last_head}" != "${curr_head}" ]
-    then
-      printf "${GREEN}%s${NORMAL}\n" "Hooray! the $pn $pt has been updated."
-    else
-      printf "${BLUE}%s${NORMAL}\n" "The $pn $pt was already at the latest version."
-    fi
+  if [ -f "${fullpath}/.noautoupdate" ]; then
+    printf "${GREEN}%s${NORMAL}\n" "Auto update of the ${pathname} ${pathtype} has been disabled (with .noautoupdate)."
   else
-    printf "${RED}%s${NORMAL}\n" "There was an error updating the $pn $pt. Try again later?"
+    last_head=$( git -C "${fullpath}" rev-parse HEAD )
+    if git -C "${fullpath}" pull --quiet --rebase --stat --autostash ; then
+      curr_head=$( git -C "${fullpath}" rev-parse HEAD )
+      if [ "${last_head}" != "${curr_head}" ]; then
+        printf "${GREEN}%s${NORMAL}\n" "Hooray! the ${pathname} ${pathtype} has been updated."
+      else
+        printf "${BLUE}%s${NORMAL}\n" "The ${pathname} ${pathtype} was already at the latest version."
+      fi
+    else
+      printf "${RED}%s${NORMAL}\n" "There was an error updating the ${pathname} ${pathtype}. Try again later?"
+    fi
   fi
 }
 
 function upgrade_oh_my_zsh_custom() {
-  if [[ -z "$ZSH_CUSTOM_AUTOUPDATE_QUIET" ]]; then
+  # Check if quiet mode is disabled
+  if [[ -z "${ZSH_CUSTOM_AUTOUPDATE_QUIET}" ]]; then
     printf "${BLUE}%s${NORMAL}\n" "Upgrading Custom Plugins"
   fi
 
-  num_workers=$( printf "%.0f" "$ZSH_CUSTOM_AUTOUPDATE_NUM_WORKERS" )
+  # Determine the number of workers for parallel updates
+  num_workers=$( printf "%.0f" "${ZSH_CUSTOM_AUTOUPDATE_NUM_WORKERS}" )
+  local worker_idx=0
   set +m
-  find -L "${ZSH_CUSTOM}" -name .git | while read d
-  do
-    if ! test $num_workers -gt 1 2> /dev/null || \
-    test $num_workers -gt 16 2> /dev/null; then
-      _upgrade_custom_plugin "${d}"
+
+  # Find all custom plugins and update them
+  find -L "${ZSH_CUSTOM}" -name .git | while read plugin_dir ; do
+    if (( num_workers <= 1 || num_workers > 16 )); then
+      _upgrade_custom_plugin "${plugin_dir}"
     else
-      ((i=(i+1)%$num_workers)) || wait
-      (_upgrade_custom_plugin "${d}") &
+      # Increment the worker index (loop around if num_workers exceeded)
+      (( worker_idx = (worker_idx + 1) % num_workers ))
+      # wait if we have looped past the end
+      if (( worker_idx == 0 )); then
+        wait
+      fi
+      # run the upgrade in the background
+      (_upgrade_custom_plugin "${plugin_dir}") &
     fi
   done
   wait
   set -m
 }
 
-alias upgrade_oh_my_zsh_all='zsh "$ZSH/tools/upgrade.sh"; upgrade_oh_my_zsh_custom'
+alias upgrade_oh_my_zsh_all='zsh "${ZSH}/tools/upgrade.sh"; upgrade_oh_my_zsh_custom'
 
 
-if [ -f ~/.zsh-custom-update ]
-then
+if [[ -f ~/.zsh-custom-update ]]; then
   mv ~/.zsh-custom-update "${ZSH_CACHE_DIR}/.zsh-custom-update"
 fi
 
 function _dispatch_update_mode() {
-  local mode
+  local mode_value
 
-  zstyle -g mode ':omz:update' mode
-  if [[ -z "$mode" ]]; then
-    if [[ "$DISABLE_AUTO_UPDATE" == "true" ]]; then
-      mode="disabled"
-    elif [[ "$DISABLE_UPDATE_PROMPT" == "true" ]]; then
-      mode="auto"
+  zstyle -g mode_value ':omz:update' mode
+  if [[ -z "${mode_value}" ]]; then
+    if [[ "${DISABLE_AUTO_UPDATE:-false}" == "true" ]]; then
+      mode_value="disabled"
+    elif [[ "${DISABLE_UPDATE_PROMPT:-false}" == "true" ]]; then
+      mode_value="auto"
     else
-      mode="prompt"
+      mode_value="prompt"
     fi
   fi
 
-  echo "$mode"
+  echo "${mode_value}"
 }
 
 update_mode="$(_dispatch_update_mode)"
 
-if [[ "$update_mode" == "disabled" ]]
-then
+if [[ "${update_mode}" == "disabled" ]]; then
   # No updates
-elif [ -f "${ZSH_CACHE_DIR}/.zsh-custom-update" ]
-then
-  . "${ZSH_CACHE_DIR}/.zsh-custom-update"
+elif [[ -f "${ZSH_CACHE_DIR}/.zsh-custom-update" ]]; then
+  source "${ZSH_CACHE_DIR}/.zsh-custom-update"
 
-  if [[ -z "$LAST_EPOCH" ]]
-  then
+  if [[ -z "${LAST_EPOCH}" ]]; then
     LAST_EPOCH=0
   fi
 
-  epoch_diff=$(($(_current_epoch) - $LAST_EPOCH))
-  if [ $epoch_diff -gt $epoch_target ]
-  then
-    if [[ "$update_mode" == "auto" ]]
-    then
+  epoch_diff=$(( $(_current_epoch) - LAST_EPOCH ))
+  if (( epoch_diff > epoch_target )); then
+    if [[ "${update_mode}" == "auto" ]]; then
       (upgrade_oh_my_zsh_custom)
-    elif [[ "$update_mode" == "reminder" ]]
-    then
+    elif [[ "${update_mode}" == "reminder" ]]; then
       echo "[oh-my-zsh] It's time to update! You can do that by running \`upgrade_oh_my_zsh_custom\`"
     else
       echo "[oh-my-zsh] Would you like to check for custom plugin updates? [Y/n]: \c"
       read line
-      if [[ "$line" == Y* ]] || [[ "$line" == y* ]] || [ -z "$line" ]
-      then
+      if [[ "${line}" == Y* || "${line}" == y* || -z "${line}" ]]; then
         (upgrade_oh_my_zsh_custom)
       fi
     fi
@@ -149,5 +152,7 @@ else
   _update_zsh_custom_update
 fi
 
-unset -f _update_zsh_custom_update _current_epoch \
-  _get_epoch_target _dispatch_update_mode
+unset -f _update_zsh_custom_update
+unset -f _current_epoch
+unset -f _get_epoch_target
+unset -f _dispatch_update_mode
